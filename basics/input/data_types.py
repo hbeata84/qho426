a000a9853641db56fc9c4ba62ade643d84987cd5
hbeata84 <84hbeata@gmail.com>
2023-06-27 11:28:13 +0100

    seminar week 1 completed

---
+++ basics/input/data_types.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("entr your age :"))
+h = float(input("Enter your height: "))
+w = float(input("Enter your weight: "))
+
+bmi = w/(h*h)
+
+print(f"{name} you are {age}years old and you BMI is {bmi}")
