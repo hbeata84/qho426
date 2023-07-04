0cbc271cb74c576f75c5f7eb5db810b332e81565
hbeata84 <84hbeata@gmail.com>
2023-07-04 11:24:24 +0100

    week 2

---
+++ basics/decision/and_operator.py
@@ -0,0 +1,7 @@
+noise = input("what did I hear? ")
+sight = input(" what did I see? ")
+
+if noise == "grrr" and sight == " Two red eyes":
+    print(" there is a scary creature. I should get out of here ")
+else:
+    print("Im a little scared but I will continue.")
---
+++ demos/W2_for.py
@@ -0,0 +1,8 @@
+for i in range(6):
+    print(f"Potato nr {i}")
+print("#"*10)
+for i in range (4,9):
+    print(f"carrot nr {i}")
+print("#"*10)
+for i in range(100, 20, -11):
+    print(f"Tomato {i}")
