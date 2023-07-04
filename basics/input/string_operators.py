4ab3023d5ca1394b655037b8c22ffc1c4fd04e6a
hbeata84 <84hbeata@gmail.com>
2023-07-04 08:24:52 +0100

    week 2 lecture

---
+++ basics/input/string_operators.py
@@ -0,0 +1,8 @@
+lives = int (input("Please enter number of lives:"))
+energy = int(input("Please enter your energy level"))
+shield = int (input("Please enter shield level"))
+
+print (" health has been set")
+print("  lives: " + "\u2764"*lives)
+print("  energy: " + "\u2756"*energy)
+print(" shield: " + "\u2742"*shield)
